f2c45190f4446620bd6e7b13c7cddeb53299f1fa
Matt Nawrocki <mjnrock@umich.edu>
2020-02-14 09:28:27 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Lux from "@lespantsfancy/lux";
+
+export default class App extends Lux.React.ReactorComponent {
+    render() {
+        return (
+            <div>
+                Hi!
+            </div>
+        );
+    }
+};
