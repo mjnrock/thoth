9e2031cc829c16aec8d082effda5d1aacb87eaf3
Matt Nawrocki <mjnrock@umich.edu>
2020-02-14 11:00:27 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
 import React from "react";
 import Lux from "@lespantsfancy/lux";
 
-let seen = [];
-let cyclic = (key, val) => {
-    if (val != null && typeof val == "object") {
-        if (seen.indexOf(val) >= 0) {
-            return;
-        }
-        seen.push(val);
-    }
-    return val;
-};
-
 export default class App extends Lux.React.ReactorComponent {
     componentDidMount() {
-        let node = new Lux.Node.Node({
-            Fish: 1
-        });
-        this.context.attach("Cat", node);
+        super.componentDidMount();
+        
+        // this.context.attach("Astro", {});
+        // this.e("Astro").$prop("astronauts", "http://api.open-notify.org/astros.json");
 
-        console.log(1111);
-        setInterval(() => {
-            this.context.getEntity("Cat").prop("Fish", this.$("Cat").Fish + 1);
-            this.forceUpdate();
-        }, 1000);
+        this.context.attach("Cats", {});
+        this.e("Cats").prop("fish", 5);
+
+        this.forceUpdate();
     }
 
     render() {
         return (
             <div>
                 <pre>
-                    {JSON.stringify(this.context, cyclic, 2)}
+                    { Lux.Core.Helper.StringifyCyclic(this.$$("Cats"), 2) }
                 </pre>
-                {/* <div>
-                    {JSON.stringify(this.context)}
-                </div> */}
             </div>
         );
     }
