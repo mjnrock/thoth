957defceb465c040a755e599d3b2aeb75a3a21f0
Matt Nawrocki <mjnrock@umich.edu>
2020-02-14 10:19:28 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
 import Lux from "@lespantsfancy/lux";
 
+let seen = [];
+let cyclic = (key, val) => {
+    if (val != null && typeof val == "object") {
+        if (seen.indexOf(val) >= 0) {
+            return;
+        }
+        seen.push(val);
+    }
+    return val;
+};
+
 export default class App extends Lux.React.ReactorComponent {
+    componentDidMount() {
+        let node = new Lux.Node.Node({
+            Fish: 1
+        });
+        this.context.attach("Cat", node);
+
+        console.log(1111);
+        setInterval(() => {
+            this.context.getEntity("Cat").prop("Fish", this.$("Cat").Fish + 1);
+            this.forceUpdate();
+        }, 1000);
+    }
+
     render() {
         return (
             <div>
-                Hi!
+                <pre>
+                    {JSON.stringify(this.context, cyclic, 2)}
+                </pre>
+                {/* <div>
+                    {JSON.stringify(this.context)}
+                </div> */}
             </div>
         );
     }
