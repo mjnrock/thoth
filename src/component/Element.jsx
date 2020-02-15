b10a25ffd0e4d20d582fab4fc9db8e083b746aa0
Matt Nawrocki <mjnrock@umich.edu>
2020-02-15 15:05:49 -0500

    init

--- src/component/Element.jsx
+++ src/component/Element.jsx
@@ -4,9 +4,7 @@ import Lux from "@lespantsfancy/lux";
 // import Core from "./../core/package";
 
 export default class Element extends Lux.React.ObserverComponent {
-    render() {
-        console.log(this.context._subject.GetChildren());
-        
+    render() {        
         return (               
             <div>
                 { this.context._subject.GetChildren().map((c, i) => (
